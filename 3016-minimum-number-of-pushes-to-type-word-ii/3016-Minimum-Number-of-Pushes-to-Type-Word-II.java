52a63860033a3a91c4c8a5573d33467a7a0b1746
Thirumurugan K <thirumurugan020070@gmail.com>
2026-07-31 09:25:20 +0530

    [3016] [Time Beats: 66.76%] [Memory Beats: 32.08%] - LeetPush

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-Minimum-Number-of-Pushes-to-Type-Word-II.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+        for (char c : word.toCharArray()) 
+        {
+            freq[c - 'a']++;
+        }
+        Arrays.sort(freq);
+        int totalPushes = 0;
+        int distinctCount = 0;
+        for (int i = 25; i >= 0; i--) 
+        {
+            if (freq[i] == 0) break;
+            int pushMultiplier = (distinctCount / 8) + 1;
+            totalPushes += freq[i] * pushMultiplier;
+            distinctCount++;
+        }
+        return totalPushes;
+    }
+}
