efaef028ce8ccfee69b58b2c326971bd5768b48a
Thirumurugan K <thirumurugan020070@gmail.com>
2026-08-06 08:49:38 +0530

    [3345] [Time Beats: 100.00%] [Memory Beats: 66.49%] - LeetPush

---
+++ 3345-smallest-divisible-digit-product-i/3345-Smallest-Divisible-Digit-Product-I.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        for(int i=n;i<=100;i++){
+            int prod=1;
+            int num=i;
+            while(num!=0){
+                prod*=(num%10);
+                num/=10;
+                if (prod==0){
+                    return i;
+                }
+            }
+            if(prod%t==0){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
