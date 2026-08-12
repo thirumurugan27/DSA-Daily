855b90ef720c2417a736d29e3a7586724e2eaa6c
Thirumurugan K <thirumurugan020070@gmail.com>
2026-08-12 09:39:50 +0530

    Time: 1279 ms (24.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,10 @@
+SELECT person_name
+FROM (
+    SELECT person_name,
+           turn,
+           SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM Queue
+) t
+WHERE total_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
