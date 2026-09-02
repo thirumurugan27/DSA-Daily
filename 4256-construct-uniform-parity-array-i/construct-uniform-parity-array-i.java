c88f7ffacaa24d9cf0f210bbe1809d2d505fec95
Thirumurugan K <thirumurugan020070@gmail.com>
2026-09-02 11:23:51 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (99.41%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,18 +1,6 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int n=nums1.length;
-        int oddC=0,evenC=0;
-        for(int i:nums1){
-            if(i%2==0) evenC++;
-            else oddC++;
-        }
-        if(oddC==n || evenC==n){
-            return true;
-        }
-        if(oddC!=0){
-            return true;
-        }
-        return false;
+        return true;
 
     }
 }
