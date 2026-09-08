157436df6a09d1f929789ee4a1fb986c7e2a7679
Thirumurugan K <thirumurugan020070@gmail.com>
2026-09-08 15:07:30 +0530

    Time: 2 ms (93.56%) | Memory: 46.5 MB (18.81%) - LeetSync

---
+++ 3533-snake-in-matrix/snake-in-matrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int finalPositionOfSnake(int n, List<String> commands) {
+        int[][] mat=new int[n][n];
+        int num=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                mat[i][j]=num++;
+            }
+        }
+        int row=0,col=0;
+        for(String s:commands){
+            if(s.equals("DOWN")){
+                row++;
+            }
+            else if(s.equals("UP")){
+                row--;
+            }
+            else if(s.equals("LEFT")){
+                col--;
+            }
+            else if(s.equals("RIGHT")){
+                col++;
+            }
+        }
+        return mat[row][col];
+    }
+}
