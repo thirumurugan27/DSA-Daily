c9221edc7338c921eaa01d0cec0ebdde20ec3d9f
Thirumurugan K <thirumurugan020070@gmail.com>
2026-09-19 19:52:01 +0530

    Time: 2 ms (97.81%) | Memory: 48.6 MB (40.87%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
@@ -15,15 +15,17 @@
  */
 class Solution {
     public void bfs(TreeNode root,List<Double> ans){
+        if(root==null) return;
         Queue<TreeNode> q=new LinkedList<>();
         q.offer(root);
         while(!q.isEmpty()){
             int n=q.size();
             double avg=0;
             for(int i=0;i<n;i++){
-                if(q.peek()!=null && q.peek().left!=null) q.offer(q.peek().left);
-                if(q.peek()!=null && q.peek().right!=null) q.offer(q.peek().right);
-                avg+=q.poll().val;
+                TreeNode node=q.poll();
+                if(node.left!=null) q.offer(node.left);
+                if(node.right!=null) q.offer(node.right);
+                avg+=node.val;
             }
             ans.add(avg/n);
         }
